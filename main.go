fed83f38163772575a0df908749843b7719370a4
estenssoros <seb.estenssoro@gmail.com>
2023-10-06 10:02:19 +0200

    refactor: use sync.waitgroup

--- main.go
+++ main.go
@@ -6,23 +6,30 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"sync"
 
 	"github.com/pkg/errors"
 )
 
-func run() error {
-	cmd := exec.Command("terraform", os.Args[1:]...)
+func run(args ...string) error {
+	cmd := exec.Command("terraform", args...)
+
+	// Create pipes for capturing stdout and stderr
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return errors.Wrap(err, "cmd.StdoutPipe")
 	}
-
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		return errors.Wrap(err, "cmd.StderrPipe")
 	}
 
+	// Create a WaitGroup to wait for goroutines to finish
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		scanner := bufio.NewScanner(stderr)
 		for scanner.Scan() {
 			m := scanner.Text()
@@ -30,20 +37,28 @@ func run() error {
 		}
 	}()
 
+	// Start the Terraform command
 	if err := cmd.Start(); err != nil {
 		return errors.Wrap(err, "cmd.Start")
 	}
 
-	scanner := bufio.NewScanner(stdout)
-	if err := runPrettier(scanner); err != nil {
+	// Capture and process stdout
+	if err := runPrettier(bufio.NewScanner(stdout)); err != nil {
 		return errors.Wrap(err, "runPrettier")
 	}
 
-	return cmd.Wait()
+	// Wait for the Terraform command to finish and wait for the stderr goroutine to finish
+	if err := cmd.Wait(); err != nil {
+		return errors.Wrap(err, "cmd.Wait")
+	}
+
+	wg.Wait() // Wait for the stderr goroutine to finish
+
+	return nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]...); err != nil {
 		log.Fatal(err)
 	}
 }
